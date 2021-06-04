98f723746ec4cbc519b969e573b58acfdcf46a90
Fidan <fidan.imanova.87@gmail.com>
2021-06-04 14:03:36 +0400

    Add project files.

---
+++ ButtonHover/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace ButtonHover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // clickbtn
+            // 
+            this.clickbtn.AutoSize = true;
+            this.clickbtn.BackColor = System.Drawing.Color.Honeydew;
+            this.clickbtn.Location = new System.Drawing.Point(321, 200);
+            this.clickbtn.Name = "clickbtn";
+            this.clickbtn.Size = new System.Drawing.Size(140, 50);
+            this.clickbtn.TabIndex = 0;
+            this.clickbtn.Text = "try to click me ";
+            this.clickbtn.UseCompatibleTextRendering = true;
+            this.clickbtn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clickbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickbtn;
+    }
+}
+
---
+++ ButtonHover/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonHover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            clickbtn.MouseHover += clickbtn_Hover;
+        }
+        private void clickbtn_Hover(object sender, EventArgs e)
+        {
+            if (sender is Button bt)
+            {
+                bt.Dispose();
+                Button button = new Button();
+                button.Size = new Size(140, 50);
+                Random randm = new Random();
+                int radomX = randm.Next(this.Size.Width - 100);
+                int radomY = randm.Next(this.Size.Height - 100);
+
+                button.Location = new Point(radomX, radomY);
+                button.Text = "try to click me";
+                button.BackColor = Color.Honeydew;
+                button.MouseHover += clickbtn_Hover;
+                this.Controls.Add(button);
+            }
+        }
+    }
+}
